cf46ae168b32eb6dbd76ff853c96d81a2fe6cdce
Utku Çetinkaya <utku.cetinkaya@hotmail.com>
2023-09-05 13:13:28 +0300

    The 1st task in Techcareer IOS Bootcamp, creating variables and assigning values, has been completed.

---
+++ Homework1.playground/Contents.swift
@@ -0,0 +1,97 @@
+import UIKit
+
+// MARK: - Constants
+
+let sehir: String = "Eskişehir"
+let ulke: String = "Türkiye"
+let telefon: Int = 05432987653
+let postaKodu: Int = 26020
+let email: String = "utku.cetinkaya@hotmail.com"
+let meslek: String = "Software Developer"
+let stokMiktari: Int = 3000
+let musteriAdi: String = "Utku Çetinkaya"
+let bakiye: Int = 200000
+let dogumGunu: String = "02/10/1999"
+let maas: Int = 25600
+let medeniDurum: String = "Bekar"
+let urunYorum: String = "Fiyat performans ürünü. Rengi çok güzel."
+let odemeTarihi: String = "05/09/2023"
+let odeme: Bool = true
+let siparisAdedi: Int = 2
+let arabaModeli: String = "Egea Cross"
+let kitapAdi: String = "George Orwel 1984"
+let yayinlanmaTarihi: String = "8 Haziran 1949"
+let indirimMiktari: Int = 35
+let odaSayisi: Int = 3
+let enlem: Float = 37.3541
+let boylam: Float = 122.1217
+let urunAdi: String = "M2 çip 13 inç MacBook Pro"
+let yemekFiyati: Float = 99.90
+let marka: String = "Apple"
+let muzikAdi: String = "Metallica - Nothing Else Matters"
+let videoSuresi: Float = 6.26
+let urunPuani: Float = 4.6
+let resimAdi: String = "Background"
+let dosyaFormati: String = "png"
+let renk: String = "Kırmızı"
+let renkKodu: String = "#E42E0E"
+let telefonModeli: String = "Apple iPhone 14 Pro (512 GB) - Uzay Siyahı"
+let ekranBoyutu: Float = 13.6
+let agirlik: Float = 1.24
+let ulusalGun: String = "19 Mayıs Atatürk'ü Anma, Gençlik ve Spor Bayramı"
+let tatilGunu: String = "Pazar"
+let rezervasyonTarihi: String = "19-23 Ağustos"
+let sokakAdi: String = "Menekşe"
+let otobusHatti: Int = 74
+let kalanDakika: Int = 13
+let takipKodu: String = "BIL357QRZ"
+let kuponSuresi: Int = 30
+let kuponKodu: String = "BUS123SAVE"
+let faturaAdresi: String = "Yaşamkent Mahallesi Çelikçiler Sokak Eskişehir/Tepebaşı"
+
+
+print("Şehir              :\(sehir)🏙️")
+print("Ülke               :\(ulke)🇹🇷")
+print("Telefon Numarası   :\(telefon)📱")
+print("Posta Kodu         :\(postaKodu)📨")
+print("E-Mail             :\(email)📩")
+print("Meslek             :\(meslek)👨‍💻")
+print("Stok Miktarı       :\(stokMiktari) Adet")
+print("Müşteri Adı        :\(musteriAdi)")
+print("Bakiye             :\(bakiye)₺")
+print("Doğum Günü         :\(dogumGunu)🎂")
+print("Maaş               :\(maas)₺")
+print("Ürün Yorumu        :\(urunYorum)")
+print("Ödeme Tarihi       :\(odemeTarihi)")
+print("Ödeme yapıldı mı?  :\(odeme)")
+print("Sipariş Adeti      :\(siparisAdedi) Adet")
+print("Araba Modeli       :\(arabaModeli)🚗")
+print("Kitap Adı          :\(kitapAdi)📚")
+print("Yayınlanma Tarihi  :\(yayinlanmaTarihi)")
+print("İndirim Miktarı    :%\(indirimMiktari)")
+print("Oda Sayısı         :\(odaSayisi) Oda")
+print("Enlem              :\(enlem)📍")
+print("Boylam             :\(boylam)📍")
+print("Ürün Adı           :\(urunAdi)")
+print("Yemek Fiyatı       :\(yemekFiyati)₺")
+print("Marka              :\(marka)")
+print("Müzik Adı          :\(muzikAdi)🎸")
+print("Video Süresi       :\(videoSuresi)sn")
+print("Ürün Puanı         :\(urunPuani)⭐")
+print("Resim Adı          :\(resimAdi)")
+print("Dosya Formatı      :\(dosyaFormati)")
+print("Renk               :\(renk)🔴")
+print("Renk Kodu          :\(renkKodu)🔴")
+print("Telefon Modeli     :\(telefonModeli)")
+print("Ekran Boyutu       :\(ekranBoyutu)inç")
+print("Ağırlık            :\(agirlik)kg")
+print("Ulusal Gün         :\(ulusalGun)🏟️")
+print("Tatil Günü         :\(tatilGunu)🏖️")
+print("Rezervasyon Tarihi :\(rezervasyonTarihi)4 Gece 5 Gün")
+print("Sokak Adı          :\(sokakAdi) Sokak")
+print("Otobüs Hattı       :\(otobusHatti) numara")
+print("Kalan Dakika       :\(kalanDakika) dk")
+print("Takip Kodu         :\(takipKodu)")
+print("Kupon Süresi       :\(kuponSuresi) gün")
+print("Kupon Kodu         :\(kuponKodu)")
+print("Fatura Adresi      :\(faturaAdresi)")
